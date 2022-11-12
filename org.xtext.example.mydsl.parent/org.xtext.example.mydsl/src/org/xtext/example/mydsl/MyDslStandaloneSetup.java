4d9f217c7583254fbb0687e3b97283757597d613
Sebastian Waldhauser <sebastian.waldhauser@gmx.at>
2022-11-12 18:53:39 +0100

    generate model separately

--- org.xtext.example.mydsl.parent/org.xtext.example.mydsl/src/org/xtext/example/mydsl/MyDslStandaloneSetup.java
+++ org.xtext.example.mydsl.parent/org.xtext.example.mydsl/src/org/xtext/example/mydsl/MyDslStandaloneSetup.java
@@ -3,13 +3,27 @@
  */
 package org.xtext.example.mydsl;
 
+import org.eclipse.emf.ecore.EPackage;
+import org.xtext.example.mydsl.myDsl.MyDslPackage;
+
+import com.google.inject.Injector;
 
 /**
- * Initialization support for running Xtext languages without Equinox extension registry.
+ * Initialization support for running Xtext languages without Equinox extension
+ * registry.
  */
 public class MyDslStandaloneSetup extends MyDslStandaloneSetupGenerated {
 
 	public static void doSetup() {
 		new MyDslStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
+
+	@Override
+	public void register(Injector injector) {
+		if (!EPackage.Registry.INSTANCE.containsKey(MyDslPackage.eNS_URI)) {
+			EPackage.Registry.INSTANCE.put(MyDslPackage.eNS_URI, MyDslPackage.eINSTANCE);
+		}
+		super.register(injector);
+	}
+
 }
