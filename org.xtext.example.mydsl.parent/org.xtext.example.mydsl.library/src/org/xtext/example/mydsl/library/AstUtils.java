0ce94ed0b41ffe8ac603cd84ca1dc7c48377392f
Sebastian Waldhauser <sebastian.waldhauser@gmx.at>
2022-11-12 22:08:49 +0100

    third party dependencies use BundleClassPath and maven-dependeny-plugin https://stackoverflow.com/questions/48050017/how-to-add-jar-dependency-to-xtext-maven-build https://stackoverflow.com/questions/6607805/how-to-copy-bundle-classpath-jars-and-their-dependencies-from-maven-repo-to-my-l

---
+++ org.xtext.example.mydsl.parent/org.xtext.example.mydsl.library/src/org/xtext/example/mydsl/library/AstUtils.java
@@ -0,0 +1,5 @@
+package org.xtext.example.mydsl.library;
+
+public class AstUtils {
+	public static void getParent() {}
+}
--- org.xtext.example.mydsl.parent/org.xtext.example.mydsl/src/org/xtext/example/mydsl/scoping/MyDslScopeProvider.java
+++ org.xtext.example.mydsl.parent/org.xtext.example.mydsl/src/org/xtext/example/mydsl/scoping/MyDslScopeProvider.java
@@ -3,6 +3,21 @@
  */
 package org.xtext.example.mydsl.scoping;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.xtext.example.mydsl.library.AstUtils;
+import org.xtext.example.mydsl.myDsl.Greeting;
+import org.xtext.example.mydsl.myDsl.Model;
 
 /**
  * This class contains custom scoping description.
@@ -11,5 +26,129 @@ package org.xtext.example.mydsl.scoping;
  * on how and when to use it.
  */
 public class MyDslScopeProvider extends AbstractMyDslScopeProvider {
-
+	private void demo() {
+		AstUtils.getParent();
+		Model m = new Model() {
+			
+			@Override
+			public void eSetDeliver(boolean deliver) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void eNotify(Notification notification) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean eDeliver() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public EList<Adapter> eAdapters() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void eUnset(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void eSet(EStructuralFeature feature, Object newValue) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public Resource eResource() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public boolean eIsSet(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean eIsProxy() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Object eInvoke(EOperation operation, EList<?> arguments) throws InvocationTargetException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Object eGet(EStructuralFeature feature, boolean resolve) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Object eGet(EStructuralFeature feature) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EList<EObject> eCrossReferences() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EList<EObject> eContents() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EReference eContainmentFeature() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EStructuralFeature eContainingFeature() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EObject eContainer() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EClass eClass() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public TreeIterator<EObject> eAllContents() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public EList<Greeting> getGreetings() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
+	}
 }
